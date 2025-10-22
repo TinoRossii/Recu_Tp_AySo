7bad7f1b561c6b202e27533024cec892b5eb61c9
TinoRossii <valentino.rossi1262@gmail.com>
2025-10-22 01:00:19 -0300

    Punto C

---
+++ Punto_C.sh
@@ -0,0 +1,3 @@
+echo "Simulación: agregar p1c1_2024_u1 al grupo p1c1_2024_Todos" >> datos/validar1.txt
+echo "Simulación: cambiar grupo propietario de datos -> p1c1_2024_Todos (con escritura)" >> datos/validar1.txt
+id >> datos/validar1.txt
